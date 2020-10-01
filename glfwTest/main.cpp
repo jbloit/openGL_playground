5140dbfa721ad7f3d3e7841d002c8fbcec1d7739
Julien Bloit <julien.bloit@gmail.com>
2020-10-01 21:18:21 +0200

    compiles, displays empty GL window.

--- glfwTest/main.cpp
+++ glfwTest/main.cpp
@@ -6,10 +6,54 @@
 //  Copyright © 2020 Julien Bloit. All rights reserved.
 //
 
-#include <iostream>
+
+/*
+ Project setup:
+ https://www.youtube.com/watch?v=4zjCqjfjcPs
+ */
+
+#include <glew.h>
+#include <glfw3.h>
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    GLFWwindow *window;
+    
+    
+    // init the library
+    if (!glfwInit())
+    {
+        return -1;
+    }
+    
+    // create windowed mode and its opengl context
+    window = glfwCreateWindow(600, 400, "Hello", nullptr, nullptr);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+    
+    // make the windows conetxt current
+    glfwMakeContextCurrent(window);
+    
+    // loop until user closes the window
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        //Render the opengl, put all your opengl code here
+        
+        
+        // swap front and back buffers
+        glfwSwapBuffers( window );
+        
+        // pool for and process events
+        glfwPollEvents();
+    }
+    
+    glfwTerminate();
+    
     return 0;
 }
