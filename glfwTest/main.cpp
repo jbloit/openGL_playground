7142aa78371622f18bfbbda548d26b4cb80adbcf
Julien Bloit <julien.bloit@gmail.com>
2020-10-01 16:34:48 +0200

    Initial Commit

---
+++ glfwTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  glfwTest
+//
+//  Created by Julien Bloit on 01/10/2020.
+//  Copyright © 2020 Julien Bloit. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
